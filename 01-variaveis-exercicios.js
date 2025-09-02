0347c642c3e95ecc74d972abc4337813a15129ad
Ricardo Nunes <ramosdasilvar@gmail.com>
2025-09-02 12:22:59 -0300

    Correção nome arquivo, inicio exercicios

---
+++ 01-variaveis-exercicios.js
@@ -0,0 +1,11 @@
+// Exercio 1 : Cadastro de usuário: Crie uma variável usuario para armazenar seu nome
+//e outra idade para sua idader. Exiba no console "O usuário [nome] tem [idade] anos"
+
+let nome = "Ricardo"
+let idade = 27
+
+console.log(`O usuário ${nome} tem ${idade} anos`)
+
+//Exercício 2: Preço com imposto: Declare uma variavel precoProduto com o preço inicial
+//de um produto e imposto com o percentual de imposto aplicado (por exemplo, 0.2 para 20%).
+//Calcule o valor total (preço inicial + imposto) e exiba o resultado.
